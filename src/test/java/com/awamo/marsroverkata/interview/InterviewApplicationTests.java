7fe98306120bea0e8de6eb92dbf46fd672b92171
Naphlin Peter Akena <starnapho@gmail.com>
2019-10-07 10:33:09 +0300

    Left direction test

--- src/test/java/com/awamo/marsroverkata/interview/InterviewApplicationTests.java
+++ src/test/java/com/awamo/marsroverkata/interview/InterviewApplicationTests.java
@@ -1,5 +1,10 @@
 package com.awamo.marsroverkata.interview;
 
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.core.Is;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +14,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class InterviewApplicationTests {
 
+	private Rover rover;
+	@Before
+	public void init() {
+		rover = new Rover();
+	}
+	
 	@Test
 	public void contextLoads() {
 	}
 
+	
+	@Test
+	public void turnRoverLeft() {
+		
+		assertThat(rover.executeCommand("L"),CoreMatchers.equalTo("0,0,W"));
+	}
 }
