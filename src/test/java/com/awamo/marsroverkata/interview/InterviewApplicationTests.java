c581b4ae1274515ebf1167984fe3c9e9165bf437
Naphlin Peter Akena <starnapho@gmail.com>
2019-10-07 14:20:57 +0300

    Corrected expected autocome from non static state

--- src/test/java/com/awamo/marsroverkata/interview/InterviewApplicationTests.java
+++ src/test/java/com/awamo/marsroverkata/interview/InterviewApplicationTests.java
@@ -3,24 +3,20 @@ package com.awamo.marsroverkata.interview;
 import static org.junit.Assert.assertThat;
 
 import org.hamcrest.CoreMatchers;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
+/**
+ * Tests for Rover commands
+ * @author naphlin
+ *
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class InterviewApplicationTests {
-
-	private Rover rover;
-	
-	@Before
-	public void init() {
-		rover = new Rover();
-	}
-	
 	
 	@Test
 	public void contextLoads() {
@@ -29,10 +25,12 @@ public class InterviewApplicationTests {
 	
 	@Test
 	public void turnRoverLeft() {
+	    
+		Rover rover = new Rover();
 		
 		assertThat(rover.executeCommand("L"),CoreMatchers.equalTo("0,0,W"));
-		assertThat(rover.executeCommand("LL"),CoreMatchers.equalTo("0,0,E"));
-		assertThat(rover.executeCommand("LLL"),CoreMatchers.equalTo("0,0,S"));
+		assertThat(rover.executeCommand("L,L"),CoreMatchers.equalTo("0,0,E"));
+		assertThat(rover.executeCommand("L,L,L"),CoreMatchers.equalTo("0,0,S"));
 		assertThat(rover.executeCommand("L"),CoreMatchers.equalTo("0,0,E"));
 			
 	}
@@ -41,9 +39,11 @@ public class InterviewApplicationTests {
 	@Test
 	public void turnRoverRight() {
 		
+		Rover rover = new Rover();
+		
 		assertThat(rover.executeCommand("R"),CoreMatchers.equalTo("0,0,E"));
-		assertThat(rover.executeCommand("RR"),CoreMatchers.equalTo("0,0,W"));
-		assertThat(rover.executeCommand("RRR"),CoreMatchers.equalTo("0,0,S"));
+		assertThat(rover.executeCommand("R,R"),CoreMatchers.equalTo("0,0,W"));
+		assertThat(rover.executeCommand("R,R,R"),CoreMatchers.equalTo("0,0,S"));
 		assertThat(rover.executeCommand("R"),CoreMatchers.equalTo("0,0,W"));
 			
 	}
@@ -52,10 +52,12 @@ public class InterviewApplicationTests {
 	@Test
 	public void moveRoverForward() {
 		
+		Rover rover = new Rover();
+		
 		assertThat(rover.executeCommand("F"),CoreMatchers.equalTo("0,1,N"));
-		assertThat(rover.executeCommand("FF"),CoreMatchers.equalTo("0,3,N"));
-		assertThat(rover.executeCommand("LFF"),CoreMatchers.equalTo("-2,3,W"));
-		assertThat(rover.executeCommand("RFF"),CoreMatchers.equalTo("-2,5,N"));
+		assertThat(rover.executeCommand("F,F"),CoreMatchers.equalTo("0,3,N"));
+		assertThat(rover.executeCommand("L,F,F"),CoreMatchers.equalTo("-2,3,W"));
+		assertThat(rover.executeCommand("R,F,F"),CoreMatchers.equalTo("-2,5,N"));
 			
 	}
 	
@@ -63,12 +65,16 @@ public class InterviewApplicationTests {
 	@Test
 	public void moveRoverBack() {
 		
-		assertThat(rover.executeCommand("LB"),CoreMatchers.equalTo("-1,0,W"));
-		assertThat(rover.executeCommand("LBB"),CoreMatchers.equalTo("-1,2,S"));
-		assertThat(rover.executeCommand("BBR"),CoreMatchers.equalTo("-1,4,W"));
-		assertThat(rover.executeCommand("FFFB"),CoreMatchers.equalTo("-3,4,W"));
+		Rover rover = new Rover();
+		
+		assertThat(rover.executeCommand("L,B"),CoreMatchers.equalTo("1,0,W"));
+		assertThat(rover.executeCommand("L,B,B"),CoreMatchers.equalTo("1,-2,S"));
+		assertThat(rover.executeCommand("B,B,R"),CoreMatchers.equalTo("1,-4,W"));
+		assertThat(rover.executeCommand("F,F,F,B"),CoreMatchers.equalTo("-1,-4,W"));
 			
 	}
+	
+	
 		
 	
 }
