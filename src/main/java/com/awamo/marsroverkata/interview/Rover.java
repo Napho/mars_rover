187c45fd3094cb669b9bd712d7c948491d7a1ccf
Naphlin Peter Akena <starnapho@gmail.com>
2019-10-07 14:22:06 +0300

    Added execution to Rover

--- src/main/java/com/awamo/marsroverkata/interview/Rover.java
+++ src/main/java/com/awamo/marsroverkata/interview/Rover.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.awamo.marsroverkata.interview.exceptions.UnknownDirectionException;
+import com.awamo.marsroverkata.interview.utils.Constants;
 import com.awamo.marsroverkata.interview.utils.Helpers;
 
 /**
@@ -19,50 +20,43 @@ import com.awamo.marsroverkata.interview.utils.Helpers;
 @Component
 public class Rover {
 
-	private static String state = "0,0,N";
-	private final int  Y_INDEX = 1;
+	private String state = "0,0,N";
+	private final int Y_INDEX = 1;
 	private final int X_INDEX = 0;
 	private final int DIRECTION_INDEX = 2;
-	
-	
-	private static final String[] COMMANDS = {"L","R","F","B"};
+
+	private static final String[] COMMANDS = { "L", "R", "F", "B" };
 	private static final String COMMAND_SEPARATOR = ",";
 
-	
 	public String executeCommand(String commands) {
-		
+
 		checkDirectionCommandIsValid(commands);
-		
+
 		String[] commandArray = commands.split(COMMAND_SEPARATOR);
-		
-		for(String command : commandArray) {
-			String[] currentState = state.split(state);
+
+		for (String command : commandArray) {
+			String[] currentState = state.split(",");
+			String direction = currentState[DIRECTION_INDEX];
 			
-			String direction = Helpers.getNewDirection(currentState[DIRECTION_INDEX], command);
-			String cordinates = Helpers.getNewCordinates(Integer.valueOf(currentState[X_INDEX]), 
-					Integer.valueOf(currentState[Y_INDEX]), currentState[DIRECTION_INDEX], command);
+			if(!(command.equals(Constants.BACKWARDS) || command.equals(Constants.FORWARD))) {
+				direction = Helpers.getNewDirection(currentState[DIRECTION_INDEX], command);
+			}
 			
-			state = cordinates+","+direction;
+			String cordinates = Helpers.getNewCordinates(Integer.valueOf(currentState[X_INDEX]),
+					Integer.valueOf(currentState[Y_INDEX]), direction, command);
+
+			state = cordinates + "," + direction;
 		}
-		
-		
-		
+
 		return state;
 
 	}
 
-	private String[] getCurrentState() {
-
-		return Rover.state.split(",");
-	}
-
-	
-
 	private void checkDirectionCommandIsValid(String command) {
 
 		final List<String> commands = Arrays.asList(COMMANDS);
-		
-		if(!Arrays.stream(command.split(COMMAND_SEPARATOR)).anyMatch(comm -> commands.contains(comm)))
+
+		if (!Arrays.stream(command.split(COMMAND_SEPARATOR)).anyMatch(comm -> commands.contains(comm)))
 			throw new UnknownDirectionException(command);
 
 	}
--- src/main/java/com/awamo/marsroverkata/interview/utils/Commands.java
+++ src/main/java/com/awamo/marsroverkata/interview/utils/Commands.java
@@ -21,6 +21,7 @@ public enum Commands {
 	 */
 	F,
 	/**
+	 * 
 	 * BACK
 	 */
 	B	
