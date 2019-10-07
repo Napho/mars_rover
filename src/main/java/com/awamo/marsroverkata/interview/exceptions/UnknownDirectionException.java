86fdf7aa8f6124ee35f99cfd2ef9c878f866a03f
Naphlin Peter Akena <starnapho@gmail.com>
2019-10-07 10:46:39 +0300

    Added wrong direction exception class

---
+++ src/main/java/com/awamo/marsroverkata/interview/exceptions/UnknownDirectionException.java
@@ -0,0 +1,29 @@
+package com.awamo.marsroverkata.interview.exceptions;
+
+/**
+ * UnknownDirectionException is raised when direction other than N,E,S,W is recieved in commands to rover
+ * @author naphlin
+ *
+ */
+public class UnknownDirectionException extends RuntimeException{
+
+	/**
+	 * Default Serial ID
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private final String command;
+	
+	public UnknownDirectionException(String command) {
+		super();	
+		this.command = command;
+		
+	}
+	
+	@Override
+	public String getMessage() {
+		return "Unknown command "+this.command+" encountered during navigation. Supported operations are E,W,N,S";
+	}
+	
+
+}
